cc55930502d1b4413d28c790d8f4740f1ba2f98e
Vitor <vitorjvbm@gmail.com>
2025-04-19 19:48:49 -0300

    fix eslint

--- src/store/reducers/produtos.ts
+++ src/store/reducers/produtos.ts
@@ -2,38 +2,38 @@ import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit'
 import { Produto } from '../../App'
 
 type ProdutosState = {
-    itens: Produto[]
+  itens: Produto[]
 }
 
 const initialState: ProdutosState = {
-    itens: []
+  itens: []
 }
 
 // crarregar produtos da api solucao
 export const carregarProdutos = createAsyncThunk(
-    'produtos/carregar',
-    async () => {
-        const resposta = await fetch(
-            'https://fake-api-tau.vercel.app/api/ebac_sports'
-        )
-        const dados: Produto[] = await resposta.json()
-        return dados
-    }
+  'produtos/carregar',
+  async () => {
+    const resposta = await fetch(
+      'https://fake-api-tau.vercel.app/api/ebac_sports'
+    )
+    const dados: Produto[] = await resposta.json()
+    return dados
+  }
 )
 
 //slice aqui
 const produtosSlice = createSlice({
-    name: 'produtos',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(
-            carregarProdutos.fulfilled,
-            (state, action: PayloadAction<Produto[]>) => {
-            state.itens = action.payload
-            }
-        )
-    }
+  name: 'produtos',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(
+      carregarProdutos.fulfilled,
+      (state, action: PayloadAction<Produto[]>) => {
+        state.itens = action.payload
+      }
+    )
+  }
 })
 
 export default produtosSlice.reducer
